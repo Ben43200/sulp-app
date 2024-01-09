caeaa4bf6af7bd626279317227537190f24206c7
meteoben <meteoben@gmail.com>
2024-01-09 09:50:51 +0100

    ajout des tarifs sous forme de liste

--- src/pages/Tarifs.js
+++ src/pages/Tarifs.js
@@ -1,9 +1,45 @@
 import React from "react"
+import "./tarifs.scss"
 
 export default function Tarifs() {
     return (
   <main>
 
+<h1 className="tarifs">TARIF DES PETITS ACTES AU 1 ER JUILLET 2023</h1>
+<ul>
+<li>NOTORIETE 350,00 € TTC</li>
+
+<li>DONATION ENTRE EPOUX (2 x 3 pages) 380,00 € TTC</li>
+
+<li>CONTRAT DE MARIAGE (sans apport) 400,00 € TTC</li>
+
+<li>TESTAMENT OLOGRAPHE (avec remise pour conservation) par personne 100,00 € TTC</li>
++ frais inscription au fichier ADSN : 11,56 € TTC par personne
++ frais conseils de rédaction : 60,00 € TTC par personne
+
+<li>TESTAMENT AUTHENTIQUE 200,00 € TTC</li>
+
+<li>DEPÔT DE TESTAMENT 360,00 € TTC</li>
+
+<li>PACS 350,00 € TTC</li>
+
+<li>CHANGEMENT DE REGIME MATRIMONIAL 1 000,00 € TTC</li>
+Le coût de l&#39;acte dépend des situations de chaque couple et de l&#39;objet du changement
+(simple clause ou changement total de régime)
++ (Si opposition) HOMOLOGATION JUDICIAIRE (frais d’avocat) 600,00 € TTC
+
+<li>MANDAT DE PROTECTION FUTURE 350,00 € TTC</li>
+
+<li>CLÔTURE D’INVENTAIRE 230,00 € TTC</li>
+
+<li>DEPÔT DE PIECES 250,00 € TTC</li>
+
+<li>DEPÔT DE CONVENTION PAR ACTE D’AVOCAT 50,00 € TTC</li>
+
+<li>ADOPTION SIMPLE 300,00 € TTC</li>
+
+<li>OPTION CONJOINT SURVIVANT 350,00 € TTC</li>
+</ul>
   
       </main>
     )
