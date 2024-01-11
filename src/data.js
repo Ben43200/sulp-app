1c29e1fcc98e86e4857c861b6fc4c4352ca7b25b
meteoben <meteoben@gmail.com>
2024-01-11 15:04:57 +0100

    suppression image du carousel dans data mêm image que le fond d'écran

--- src/data.js
+++ src/data.js
@@ -2,6 +2,6 @@ export const stockData = [
     
     "https://lh3.googleusercontent.com/p/AF1QipOiCGWMYwqakmaRTTBmEprsBZKdeSTz4INEAjss=s1766-w1766-h1716-rw",
     "https://lh3.googleusercontent.com/p/AF1QipPpHcMRo4tiiJ4CSYI4uEGW3WtGkjwowqR92Qu9=s1766-w1766-h1716-rw",
-    "https://lh3.googleusercontent.com/p/AF1QipN6SOZdPb4Ctkk0WYbzQnHSG-XBUuKtV12Ni4ne=s1766-w1766-h1716-rw",
-    "https://lh3.googleusercontent.com/p/AF1QipN49M4LstGnW3MalTVa8mfobs5pY2dAADNb8qdJ=s1766-w1766-h1716-rw"
+    "https://lh3.googleusercontent.com/p/AF1QipN6SOZdPb4Ctkk0WYbzQnHSG-XBUuKtV12Ni4ne=s1766-w1766-h1716-rw"
+    // "https://lh3.googleusercontent.com/p/AF1QipN49M4LstGnW3MalTVa8mfobs5pY2dAADNb8qdJ=s1766-w1766-h1716-rw"
   ];
