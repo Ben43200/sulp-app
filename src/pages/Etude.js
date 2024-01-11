8d5263e55a158423a5812cd044587205828c9e45
meteoben <meteoben@gmail.com>
2024-01-11 14:10:10 +0100

    ajout page étude positionnement carousel en grid fond d'écran sur tout les main

--- src/pages/Etude.js
+++ src/pages/Etude.js
@@ -1,10 +1,11 @@
 import React from "react"
 import { stockData } from "../data"
 import Carousel from "../components/Carousel"
+import "./etude.scss"
 
 export default function Etude() {
     return (
-  <main>
+<main className="etude-container">
   {/* <div className="logo-container"> <img src={logo} className='logo-1' alt="logo" />
   <img src={logonotaire} className='logo-not' alt="logo" />
   </div> */}
