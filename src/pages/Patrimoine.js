21d5bbfc10fd4811c38e2da15eb59ad4ca197f74
meteoben <meteoben@gmail.com>
2024-01-16 19:20:32 +0100

    remplissage contenu page patrimoine ok

--- src/pages/Patrimoine.js
+++ src/pages/Patrimoine.js
@@ -7,6 +7,16 @@ export default function Patrimoine(){
     return(
         <main>
         <MenuCompetences />
+        <h1>Le patrimoine</h1>
+        <p>Testament, donation, partage de la succession, relations entre les héritiers... Pas toujours facile de s'y
+retrouver et de ne pas faire d'erreur. Votre notaire saura vous guider pour réussir au mieux la
+transmission de votre patrimoine et éviter les conflits familiaux.</p>
+<ul>
+    <li>Succession</li>
+    <li>Les réglements de la succession</li>
+    <li>Donation, donation-partage</li>
+    <li>Les testaments</li>
+</ul>
         </main>
         )
         }
