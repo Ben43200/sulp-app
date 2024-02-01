ff2dedb924db46d8a386887f53765cb056161090
meteoben <meteoben@gmail.com>
2024-02-01 16:21:16 +0100

    ajout info contact page home

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./contact.scss";
+
+
+
+export default function Contact() {
+  return (
+    <div className="contact-container">
+      <h3>Me Jean-François SULPICE</h3>
+      <p>Adresse: 4 Place du Chapitre</p>
+      <p>01400 Neuville-les-Dames</p>
+      <p>Téléphone : 04 74 42 87 95</p>
+    </div>
+  );
+}
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,23 +1,25 @@
 import React from "react";
 import Carousel from "../../components/Carousel/Carousel";
-import {stockData} from "../../data"
+import { stockData } from "../../data";
 import "./home.scss";
+import Contact from "../../components/Contact/Contact";
 // import ContactForm from "../../components/Contact/ContactForm";
 
-import font from "../../Utils/Façade_8.webp"
-
+// import font from "../../Utils/Façade_8.webp";
 
 export default function Home() {
   return (
-<main className="home-container">
-{/* <div className="home-font">
+    <main className="home-container">
+      {/* <div className="home-font">
   <img src={font} />
   </div> */}
-<div className="carousel-container">
-  <Carousel images={stockData} />
- 
-  </div>
-
+      <div className="carousel-container">
+        <Carousel images={stockData} />
+      </div>
+      {/* <div className="home-font">
+  <img src={font} />
+  </div> */}
+      <Contact />
     </main>
-  )
+  );
 }
