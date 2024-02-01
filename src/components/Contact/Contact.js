e38dd6624928036f30527b2e1e6bf7e435203f84
meteoben <meteoben@gmail.com>
2024-02-01 16:57:31 +0100

    séparation lien tel

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -9,7 +9,7 @@ export default function Contact() {
       <h3>Me Jean-François SULPICE</h3>
       <p>Adresse: 4 Place du Chapitre</p>
       <p>01400 Neuville-les-Dames</p>
-      <p>Téléphone :<a href="tel:0474428795">04 74 42 87 95</a></p>
+      <p>Téléphone : <a href="tel:0474428795">04 74 42 87 95</a></p>
     </div>
   );
 }
