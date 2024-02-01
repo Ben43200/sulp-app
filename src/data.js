63e927c2f01e92cd4b2064f8d8a342304852c7ca
meteoben <meteoben@gmail.com>
2024-02-01 16:53:43 +0100

    mise en commentaires des images non libre de droit dans data.js

--- src/data.js
+++ src/data.js
@@ -4,13 +4,13 @@ export const stockData = [
     // "https://lh3.googleusercontent.com/p/AF1QipPpHcMRo4tiiJ4CSYI4uEGW3WtGkjwowqR92Qu9=s1766-w1766-h1716-rw",
     // "https://lh3.googleusercontent.com/p/AF1QipN6SOZdPb4Ctkk0WYbzQnHSG-XBUuKtV12Ni4ne=s1766-w1766-h1716-rw",
     "https://meteoben.eu/Utils_Jeff/Façade_1.JPG",
-    "https://meteoben.eu/Utils_Jeff/Eglise-de-profil.jpg",
+    // "https://meteoben.eu/Utils_Jeff/Eglise-de-profil.jpg",
     "https://meteoben.eu/Utils_Jeff/Bureau_1.JPG",
     "https://meteoben.eu/Utils_Jeff/Salle_d'attente_2.JPG",
-    "https://meteoben.eu/Utils_Jeff/ancien-chateau-de-chevigney.jpg",
+    // "https://meteoben.eu/Utils_Jeff/ancien-chateau-de-chevigney.jpg",
 
 
-    "https://meteoben.eu/Utils_Jeff/raison-notariat.jpg"
+    // "https://meteoben.eu/Utils_Jeff/raison-notariat.jpg"
 
     // "https://lh3.googleusercontent.com/p/AF1QipN49M4LstGnW3MalTVa8mfobs5pY2dAADNb8qdJ=s1766-w1766-h1716-rw"
   ];
