e315c34583449569ec7988fc4da4bc0b826f2711
meteoben <meteoben@gmail.com>
2024-02-01 11:21:05 +0100

    ajout mentions suite sans erreurs crédit photos infos concepteur du site

--- src/pages/MentionsLegales/MentionsLegales.js
+++ src/pages/MentionsLegales/MentionsLegales.js
@@ -5,76 +5,92 @@ export default function MentionsLegales() {
   return (
     <main>
       <h2 className="mentions-title">Mentions Légales</h2>
+      <h3 className="mentions-subtitle">1Infos Editeur</h3>
+      <p className="mentions-details">
+        Éditeur : le Site est conçu par Benjamin Dollfus, micro-entrepreneur,
+        Immatriculée sous le n° Siret 514 928 666 000 48 - Code APE 6201Z - 8B
+        rue des Jardins 26400 Aouste-sur-sye. Tél. 06.98.55.70.26. Email :
+        meteoben@gmail.com.
+      </p>
       <h3 className="mentions-subtitle">
-        3.3. Conception et réalisation du site de Maître SULPICE</h3>
-       <p className="mentions-details">
-       Benjamin Dollfus est le propriétaire
-        exclusif de l’ensemble des éléments ayant permis la conception et la
-        réalisation du site.</p>
-        <p className="mentions-details">La reproduction sur support électronique La
-        reproduction de tout ou partie de ce site sur un support électronique
-        est autorisée sous réserve de l'ajout de façon claire et lisible de la
-        source (URL exacte du site) et de la mention "Droits réservés". Les
+        2. Conception et réalisation du site de Maître SULPICE
+      </h3>
+      <p className="mentions-details">
+        Benjamin Dollfus est le propriétaire exclusif de l’ensemble des éléments
+        ayant permis la conception et la réalisation du site.
+      </p>
+      <p className="mentions-details">
+        La reproduction sur support électronique La reproduction de tout ou
+        partie de ce site sur un support électronique est autorisée sous réserve
+        de l'ajout de façon claire et lisible de la source (URL exacte du site)
+        et de la mention "Droits réservés". Les informations utilisées ne
+        doivent l'être qu'à des fins personnelles, associatives ou
+        professionnelles ; toute utilisation à des fins commerciales ou
+        publicitaires est exclue. Toute reproduction, autrement que pour l'usage
+        privé de l'utilisateur du site en vue notamment d'une diffusion publique
+        par n'importe quel moyen est strictement interdite sans l'autorisation
+        préalable et écrite de Benjamin Dollfus.
+      </p>
+      <p className="mentions-details">
+        La création de liens hypertextes vers le site :
+        https://adresse-sulpice.notaires.fr Le site autorise la mise en place
+        d’un lien hypertexte pointant vers son contenu, sous réserve d’un accord
+        préalable et exprès de Benjamin Dollfus de :
+      </p>
+      <p className="mentions-details">
+        ne pas utiliser la technique du lien profond ("deep linking"),
+        c'est-à-dire que les pages du site https://adresse_sulpice.notaires.fr
+        ne doivent pas être imbriquées à l'intérieur des pages d'un autre site,
+        mais accessible par l’ouverture d’une fenêtre, mentionner la source qui
+        pointera grâce à un lien hypertexte directement sur le contenu visé, les
         informations utilisées ne doivent l'être qu'à des fins personnelles,
-        associatives ou professionnelles ; toute utilisation à des fins
-        commerciales ou publicitaires est exclue. Toute reproduction, autrement
-        que pour l'usage privé de l'utilisateur du site en vue notamment d'une
-        diffusion publique par n'importe quel moyen est strictement interdite
-        sans l'autorisation préalable et écrite de Benjamin Dollfus.
-        </p> 
-        
-        <p className="mentions-details">
-        
-        La création de liens
-        hypertextes vers le site : https://adresse-sulpice.notaires.fr Le site autorise la
-        mise en place d’un lien hypertexte pointant vers son contenu, sous
-        réserve d’un accord préalable et exprès de Benjamin Dollfus de :
-        </p>
-         <p className="mentions-details"> 
-         ne pas utiliser la
-        technique du lien profond ("deep linking"), c'est-à-dire que les pages
-        du site https://adresse_sulpice.notaires.fr ne doivent pas être imbriquées à
-        l'intérieur des pages d'un autre site, mais accessible par l’ouverture
-        d’une fenêtre, mentionner la source qui pointera grâce à un lien
-        hypertexte directement sur le contenu visé, les informations utilisées
-        ne doivent l'être qu'à des fins personnelles, associatives ou
-        professionnelles, toute utilisation à des fins commerciales ou
-        publicitaires est exclue.
-        </p>
-        <p className="mentions-details">
-          Attention ! Cette autorisation ne s’applique
-        pas aux sites internet diffusant des informations à caractère polémique,
-        pornographique, xénophobe ou pouvant, dans une plus large mesure porter
-        atteinte à la sensibilité du plus grand nombre. L’Editeur décline toute
-        responsabilité en ce qui concerne les pratiques et les politiques de
-        protection de la confidentialité des données des sites auxquels nous
-        pouvons être reliés par des liens hypertextes et nous vous engageons à
-        en prendre connaissance.
+        associatives ou professionnelles, toute utilisation à des fins
+        commerciales ou publicitaires est exclue.
       </p>
-      <h3 className="mentions-subtitle">
-        4. Contenus du site</h3> <p className="mentions-details">Les informations contenues sur le site engagent
-        seule la responsabilité de leur auteur et ne sauraient engager la
-        responsabilité de l’Editeur lorsque ce dernier n’est pas l’auteur de ces
-        informations. L’Editeur ne donne aucune garantie, explicite ou
-        implicite, et n'assume aucune responsabilité relative à l'utilisation de
-        la présente publication. En aucun cas, l’Editeur ou un tiers impliqué
-        dans la création de ce site ne sont responsables, ni ne peuvent être
-        redevables à un utilisateur ou une autre partie, des dommages directs ou
-        indirects, spéciaux, particuliers ou accessoires découlant de
-        l'utilisation de ce site ou d'un autre site relié par un hyperlien,
-        notamment des pertes de profit, de l'interruption des affaires, des
-        pertes de programmes ou d'autres données du système de gestion de
-        l'information de l'utilisateur ou autre, même si l’Editeur est
-        expressément informé que des dommages sont possibles. Votre connexion à
-        tout autre site extérieur est à vos risques exclusifs. La reproduction
-        et la diffusion de l'image ou la vidéo d'une personne doivent respecter
-        les principes issus du droit à l'image et du droit à la vie privée.
-        L’Editeur ne donne pas de garantie quant à la qualité, l'exactitude,
-        l'actualité, l'ordre ou l'exhaustivité de l'information ou des documents
-        dans ce site, et n'est tenu à aucune mise à jour de ces informations.
-        Les autres documents sont présentés à titre informatif et ne sauraient
-        engager l’Editeur.
+      <p className="mentions-details">
+        Attention ! Cette autorisation ne s’applique pas aux sites internet
+        diffusant des informations à caractère polémique, pornographique,
+        xénophobe ou pouvant, dans une plus large mesure porter atteinte à la
+        sensibilité du plus grand nombre. L’Editeur décline toute responsabilité
+        en ce qui concerne les pratiques et les politiques de protection de la
+        confidentialité des données des sites auxquels nous pouvons être reliés
+        par des liens hypertextes et nous vous engageons à en prendre
+        connaissance.
       </p>
+      <h3 className="mentions-subtitle">3. Contenus du site</h3>{" "}
+      <p className="mentions-details">
+        Les informations contenues sur le site engagent seule la responsabilité
+        de leur auteur et ne sauraient engager la responsabilité de l’Editeur
+        lorsque ce dernier n’est pas l’auteur de ces informations. L’Editeur ne
+        donne aucune garantie, explicite ou implicite, et n'assume aucune
+        responsabilité relative à l'utilisation de la présente publication. En
+        aucun cas, l’Editeur ou un tiers impliqué dans la création de ce site ne
+        sont responsables, ni ne peuvent être redevables à un utilisateur ou une
+        autre partie, des dommages directs ou indirects, spéciaux, particuliers
+        ou accessoires découlant de l'utilisation de ce site ou d'un autre site
+        relié par un hyperlien, notamment des pertes de profit, de
+        l'interruption des affaires, des pertes de programmes ou d'autres
+        données du système de gestion de l'information de l'utilisateur ou
+        autre, même si l’Editeur est expressément informé que des dommages sont
+        possibles. Votre connexion à tout autre site extérieur est à vos risques
+        exclusifs. La reproduction et la diffusion de l'image ou la vidéo d'une
+        personne doivent respecter les principes issus du droit à l'image et du
+        droit à la vie privée. L’Editeur ne donne pas de garantie quant à la
+        qualité, l'exactitude, l'actualité, l'ordre ou l'exhaustivité de
+        l'information ou des documents dans ce site, et n'est tenu à aucune mise
+        à jour de ces informations. Les autres documents sont présentés à titre
+        informatif et ne sauraient engager l’Editeur.
+      </p>
+      <h3 className="mentions-subtitle">4. Crédits photos</h3>
+      <p className="mentions-details">
+        Le concepteur du site s’engage à garantir que les photographies et/ou vidéos ajoutées sont
+        libres de droit ou dans le cas contraire à mentionner le nom de l’auteur
+        du document.</p>
+        <ul>
+       <li>©Jean-François Sulpice</li>
+       <li>©Benjamin Dollfus</li>
+        </ul>
+      
     </main>
   );
 }
