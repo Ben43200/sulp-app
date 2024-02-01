25f0973f2d4f0de45703f4111e4fd3e523fdff4a
meteoben <meteoben@gmail.com>
2024-02-01 14:48:44 +0100

    suppression formulaire de contact et fond d'écran de main

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,21 +2,22 @@ import React from "react";
 import Carousel from "../../components/Carousel/Carousel";
 import {stockData} from "../../data"
 import "./home.scss";
-import ContactForm from "../../components/Contact/ContactForm";
+// import ContactForm from "../../components/Contact/ContactForm";
 
-
+import font from "../../Utils/Façade_8.webp"
 
 
 export default function Home() {
   return (
 <main className="home-container">
+{/* <div className="home-font">
+  <img src={font} />
+  </div> */}
 <div className="carousel-container">
   <Carousel images={stockData} />
  
   </div>
-  <div className="contact-form-container">
-  <ContactForm />
-  </div>
+
     </main>
   )
 }
