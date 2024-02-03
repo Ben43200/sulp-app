c214990df734dc289df5d74cd2531681a99b822e
meteoben <meteoben@gmail.com>
2024-02-03 06:35:00 +0100

    implémentation des p dans compétences et étude h2 en black

--- src/pages/Competences/Entreprise/Entreprise.js
+++ src/pages/Competences/Entreprise/Entreprise.js
@@ -8,8 +8,8 @@ export default function Entreprise(){
         <main>
             <MenuCompetences />
             <h1>Le droit de l'entreprise</h1>
-            <p>Les notaires offrent des prestations de conseils aux chefs d'entreprise.
-Ils abordent le droit de l'entreprise dans tous ses aspects : choix de la forme la mieux adaptée au
+            <p>Les notaires offrent des prestations de conseils aux chefs d'entreprise.</p>
+<p>Ils abordent le droit de l'entreprise dans tous ses aspects : choix de la forme la mieux adaptée au
 projet, rédaction des statuts, mais aussi rédaction des baux commerciaux, mise en place de location-
 gérance ou encore vente de fonds de commerce.</p>
 <div className="img-container">
--- src/pages/Competences/Patrimoine/Patrimoine.js
+++ src/pages/Competences/Patrimoine/Patrimoine.js
@@ -10,10 +10,10 @@ export default function Patrimoine(){
         <h1>Le patrimoine</h1>
         <p>Placement immobilier, épargne-retraite, stratégie de transmission, optimisation fiscale, testament,
 donation, protection du conjoint, partage de la succession, relations entre les héritiers... Pas toujours
-facile de s'y retrouver et de ne pas faire d'erreur.
-Votre notaire saura vous guider pour réussir au mieux la transmission de votre patrimoine et éviter
-les conflits familiaux.
-Quel que soit votre objectif, votre notaire saura donner vie à vos projets.</p>
+facile de s'y retrouver et de ne pas faire d'erreur.</p>
+<p>Votre notaire saura vous guider pour réussir au mieux la transmission de votre patrimoine et éviter
+les conflits familiaux.</p>
+<p>Quel que soit votre objectif, votre notaire saura donner vie à vos projets.</p>
 <div className="img-container">
 <img className="img-comp" src={Money} alt="Patrimoine"/>
 </div>
