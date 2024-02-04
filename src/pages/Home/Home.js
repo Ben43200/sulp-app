755756ba58c91596f31cd47c58773c4456f6351e
meteoben <meteoben@gmail.com>
2024-02-04 09:25:25 +0100

    implémentation du lien en target blank

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,6 +4,7 @@ import { stockData } from "../../data";
 import "./home.scss";
 import Contact from "../../components/Contact/Contact";
 import Google from "../../Utils/google-map.png";
+import { Link } from "react-router-dom";
 // import ContactForm from "../../components/Contact/ContactForm";
 
 // import font from "../../Utils/Façade_8.webp";
@@ -11,9 +12,13 @@ import Google from "../../Utils/google-map.png";
 export default function Home() {
   return (
     <main className="home-container">
+       
       <div className="home-font">
+      <Link to="https://maps.app.goo.gl/UBfWP5JLSstGKZf29" target="_blank" rel="noopener noreferrer">
         <img className="carte" src={Google} alt="carte" />
+        </Link>
       </div>
+      
       <div className="carousel-container">
         <Carousel fetchpriority="high" images={stockData} />
       </div>
