c67c917dc672ed488bf9e5e79aa4f512dffeac45
meteoben <meteoben@gmail.com>
2024-02-05 04:03:18 +0100

    mise en place du titre du titre

--- src/pages/Contact/ContactPage.js
+++ src/pages/Contact/ContactPage.js
@@ -14,10 +14,14 @@ export default function ContactPage(){
       <p>01400 Neuville-les-Dames</p>
       <p>Téléphone : <a href="tel:0474428795">04 74 42 87 95</a></p>
     </div> */}<div className="contact-page-container">
+        <div className="contact-title">
+  <h1>Contactez votre notaire</h1>
+  </div>
           <div className="home-font">
           <Link to="https://maps.app.goo.gl/UBfWP5JLSstGKZf29" target="_blank" rel="noopener noreferrer">
   <img className="carte" src={Google} alt="carte"/>
   </Link>
+
   </div>
     <Contact />
     </div>
