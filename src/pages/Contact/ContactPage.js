30d6edc6492c0d679277690da72dfef466742664
meteoben <meteoben@gmail.com>
2024-02-05 11:32:18 +0100

    modif aside contact page left column ok

--- src/pages/Contact/ContactPage.js
+++ src/pages/Contact/ContactPage.js
@@ -7,16 +7,10 @@ import { Link } from "react-router-dom";
 
 export default function ContactPage(){
     return(
-        <main>
-    {/* <div className="contact-container">
-      <h3>Me Jean-François SULPICE</h3>
-      <p>Adresse: 4 Place du Chapitre</p>
-      <p>01400 Neuville-les-Dames</p>
-      <p>Téléphone : <a href="tel:0474428795">04 74 42 87 95</a></p>
-    </div> */}<div className="contact-page-container">
-        <div className="contact-title">
-  <h1>Contactez votre notaire</h1>
-  </div>
+        <main className="contact-page-container">
+
+
+  <aside className="left-column">
           <div className="home-font">
           <Link to="https://maps.app.goo.gl/UBfWP5JLSstGKZf29" target="_blank" rel="noopener noreferrer">
   <img className="carte" src={Google} alt="carte"/>
@@ -24,8 +18,12 @@ export default function ContactPage(){
 
   </div>
     <Contact />
-    </div>
-    
+    </aside>
+    <aside className="right-column">
+    <div className="contact-title">
+  <h1>Contactez votre notaire</h1>
+  </div>
+  </aside>
         </main>
         )
         }
