73aa899bfc0d0adaeca9b2c5ac544cbbfa744c93
meteoben <meteoben@gmail.com>
2024-02-05 18:16:42 +0100

    ajout page tarif avec liens pdf

--- src/pages/Tarifs/Tarifs.js
+++ src/pages/Tarifs/Tarifs.js
@@ -57,32 +57,63 @@ import "./tarifs.scss";
 //   );
 // }
 
-
-import React, { Component } from 'react';
-import Pdf1 from '../../Utils/Tarifs.pdf';
-import Pdf2 from "../../Utils/Tableau.pdf"
+import React, { Component } from "react";
+import Pdf1 from "../../Utils/GENERALITES SUR LE TARIF DU NOTAIRE. EMOLUMENTS ET HONORAIRES.pdf";
+import Pdf2 from "../../Utils/Tarifs_affiches.pdf";
+import Pdf3 from "../../Utils/Tableau.pdf";
 import "./tarifs.scss";
 
-export  class Tarif extends Component {
-
+export class Tarif extends Component {
   render() {
-
     return (
       <main>
-        <ul>
-        <li className = "tarifs">
-          <a href = {Pdf1} target = "_blank"><p>Téléchargez les tarifs affichés en salle d'attente</p></a>
-        </li>
-          <li className = "tarifs">
-          <a href = {Pdf2} target = "_blank"><p>Téléchargez le Tableau des honoraires</p></a>
-        </li>
-        </ul>
-        </main>
-    );
+        <h3>LE TARIF DES NOTAIRES</h3>
+        <p>
+          Il convient de distinguer les actes tarifés (ventes, donations,
+          contrats de mariage, successions…) pour lesquels le notaire perçoit
+          des émoluments (dont le tarif est fixé réglementairement par l’Etat),
+          des actes non tarifés qui font l’objet d’honoraires libres.
+        </p>
+        <h4>Généralités sur le tarif des notaires</h4>
 
+        <a href={Pdf1} target="_blank">
+          <p>Téléchargez ici le document sur les Généralités sur le tarif des notaires</p>
+        </a>
+
+        <h4>Le tarif affiché dans la salle d’attente de l’Office</h4>
+        <a href={Pdf2} target="_blank">
+          <p>Téléchargez ici les tarifs affichés en salle d'attente</p>
+        </a>
+
+        <h3>LES HONORAIRES</h3>
+
+        <p>
+          Depuis le 1er mars 2016, les activités de négociation immobilière et
+          de transaction donnent désormais lieu à des versements d’honoraires.
+          Il en est de même pour les consultations des clients relatives à des
+          prestations qui sont détachables de la préparation, de la rédaction ou
+          de l’exécution de l’acte.
+        </p>
+        <p>
+          Obligation : La fixation de ces honoraires donne lieu à une convention
+          signée entre le notaire et son client.
+        </p>
+
+        <h4>Honoraires de l’Office</h4>
+
+        <ul className="honoraires-list">
+          <li>
+            Tarifs d'honoraires de négociation 4.5 % TTC maximum du prix net
+            vendeur Avec un minimum de 3600 € TTC.
+          </li>
+          <li>Tarifs d’expertise sur devis seulement.</li>
+        </ul>
+        <a href={Pdf3} target="_blank">
+          <p>Téléchargez ici le tableau des honoraires</p>
+        </a>
+      </main>
+    );
   }
 }
 
 export default Tarif;
-
-
